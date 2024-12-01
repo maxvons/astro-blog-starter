92e04bfa51f5900341ff39e3b075bca163563979
Maximilian von Stephanides <maximilianvons@gmail.com>
2024-12-01 15:57:02 +0100

    Add comment to config option to better clarify usage

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://example.com',  // Enter the URL of your deployed site here
 	integrations: [mdx(), sitemap()],
 });

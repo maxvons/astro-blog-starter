4986695716b4b6e0eea368bb09a4a90c97afd6b7
Maximilian von Stephanides <maximilianvons@gmail.com>
2024-12-01 16:08:13 +0100

    Add prettier as dev dependency and add prettier config.
    - prettier is strictly speaking not necessary if you're using an editor like VS Code with the Astro extension, but this adds formatting support through cli.

--- astro.config.js
+++ astro.config.js
@@ -1,10 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',  // Enter the URL of your deployed site here
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com", // Enter the URL of your deployed site here
+  integrations: [mdx(), sitemap()],
 });

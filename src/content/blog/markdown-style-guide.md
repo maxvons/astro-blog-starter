e67a6c405dd992625b097eb3c3fadf85e4ba0f91
Maximilian von Stephanides <maximilianvons@gmail.com>
2024-12-03 21:44:34 +0100

    Some changes

--- src/content/blog/markdown-style-guide.md
+++ src/content/blog/markdown-style-guide.md
@@ -2,7 +2,8 @@
 title: "Markdown Style Guide"
 description: "Here is a sample of some basic Markdown syntax that can be used when writing Markdown content in Astro."
 pubDate: "Jun 19 2024"
-heroImage: "/images/blog-placeholder-1.jpg"
+cover: "./images/blog-placeholder-1.jpg"
+coverAlt: "Alt text"
 ---
 
 Here is a sample of some basic Markdown syntax that can be used when writing Markdown content in Astro.
--- src/content/config.ts
+++ src/content/config.ts
@@ -3,14 +3,19 @@ import { defineCollection, z } from "astro:content";
 const blog = defineCollection({
   type: "content",
   // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      cover: image()
+        .refine((img) => img.width >= 960, {
+          message: "Cover image must be at least 1080 pixels wide!",
+        })
+        .optional(),
+      coverAlt: z.string().optional(),
+    }),
 });
 
 export const collections = { blog };

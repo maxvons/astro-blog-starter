6d1b081bd2d5c9b0c40347730862ba0e0605aed5
Maximilian von Stephanides <maximilianvons@gmail.com>
2024-12-03 15:52:25 +0100

    Add astro-icon dependency
    - astro-icon makes it easy to use and customize icons in an Astro project.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com", // CUSTOM: Enter the URL of your deployed site here
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), icon()],
 });
